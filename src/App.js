86e141d50aed27bab4fc27b716e3a2d7e44f1459
KairatTursunbekov <117997942+KairatTursunbekov@users.noreply.github.com>
2023-12-20 11:20:49 +0600

    added TodoItem component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-
+import { useState } from "react"
+import { TodoItem } from "./Components/TodoItem"
 
 function App(){
+    const [todos, setTodos]= useState([])
+    const [inputValue, setInputValue]= useState('')
+
+    const createTodo = ()=> {
+        setTodos([...todos, {text: inputValue, complete: true}])
+        setInputValue("")
+    }
     return (
-        <div className = "App">Master</div>
+        <div className = "App">
+            <div>
+                <input type="text" 
+                    value={inputValue}
+                    onChange={(e)=> {
+                    setInputValue(e.target.value)
+                }}/>
+                <button onClick={createTodo}>ADD</button>
+            </div>
+            <div>
+                {
+                    todos.map((el,id)=> {
+                        return<TodoItem todo= {el} key={id}/>
+                    })
+                }
+            </div>
+        </div>
     )
 }
 export default App
